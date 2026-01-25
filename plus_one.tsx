9d1da1f9ed14b32301a5d0cccaecd08969db1c33
srikarreddy1998 <master.srikarreddy@gmail.com>
2026-01-25 23:55:15 +0530

    plus one (math & geometry - EASY)

---
+++ plus_one.tsx
@@ -0,0 +1,20 @@
+one;
+class Solution {
+  /**
+   * @param digits
+   * @return digits after +1
+   */
+  plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i] += 1;
+        return digits;
+      }
+      digits[i] = 0; // carry over
+    }
+
+    // If we reach here, all digits were 9
+    digits.unshift(1);
+    return digits;
+  }
+}
